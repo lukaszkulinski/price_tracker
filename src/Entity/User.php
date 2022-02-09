b61f4021f48920f38b596a0f87116b027cdaa0cb
Łukasz <lukasz.kulinski8@gmail.com>
2022-02-09 20:40:35 +0100

    Add email field

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -24,6 +24,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'string')]
     private $password;
 
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
+    private $email;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -92,5 +95,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
     }
 }
